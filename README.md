199fbebb1f928cf426d263f46db685fbe68b7903
Prachi Damle <prachi@rancher.com>
2020-08-13 17:36:39 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# clusterscan-operator
---
+++ main.go
@@ -0,0 +1,83 @@
+//go:generate go run pkg/codegen/cleanup/main.go
+//go:generate /bin/rm -rf pkg/generated
+//go:generate go run pkg/codegen/main.go
+
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/rancher/wrangler/pkg/kubeconfig"
+	"github.com/rancher/wrangler/pkg/signals"
+	"github.com/sirupsen/logrus"
+	"github.com/urfave/cli"
+	"github.com/prachidamle/clusterscan-operator/pkg/clusterscan-operator"
+	cisoperatorapiv1 "github.com/prachidamle/clusterscan-operator/pkg/apis/clusterscan-operator.cattle.io/v1"
+	"time"
+)
+
+var (
+	Version    = "v0.0.0-dev"
+	GitCommit  = "HEAD"
+	KubeConfig string
+	threads int
+	name string
+)
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "clusterscan-operator"
+	app.Version = fmt.Sprintf("%s (%s)", Version, GitCommit)
+	app.Usage = "clusterscan-operator needs help!"
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:        "kubeconfig",
+			EnvVar:      "KUBECONFIG",
+			Destination: &KubeConfig,
+		},
+		cli.IntFlag{
+			Name:        "threads",
+			EnvVar:      "CIS_OPERATOR_THREADS",
+			Value:       2,
+			Destination: &threads,
+		},
+		cli.StringFlag{
+			Name:        "name",
+			EnvVar:      "CIS_OPERATOR_NAME",
+			Value:       "clusterscan-operator",
+			Destination: &name,
+		},
+	}
+	app.Action = run
+
+	if err := app.Run(os.Args); err != nil {
+		logrus.Fatal(err)
+	}
+}
+
+func run(c *cli.Context) {
+	flag.Parse()
+
+	logrus.Info("Starting ClusterScan-Operator")
+	ctx := signals.SetupSignalHandler(context.Background())
+
+	kubeConfig, err := kubeconfig.GetNonInteractiveClientConfig(KubeConfig).ClientConfig()
+	if err != nil {
+		logrus.Fatalf("failed to find kubeconfig: %v", err)
+	}
+
+	ctl, err := clusterscan_operator.NewController(kubeConfig, ctx, cisoperatorapiv1.ClusterScanNS, name)
+	if err != nil {
+		logrus.Fatalf("Error building controller: %s", err.Error())
+	}
+	logrus.Info("Registering ClusterScan controller")
+
+	if err := ctl.Start(ctx, threads, 2*time.Hour); err != nil {
+		logrus.Fatalf("Error starting: %v", err)
+	}
+	<-ctx.Done()
+	logrus.Info("Registered ClusterScan controller")
+}
