3a368d9a2b43cdc96a1193e53b458fecbf684294
Paulo Gomes <pjbgf@linux.com>
2024-08-15 20:33:37 +0100

    tests: Bump kubectl to v1.28.12
    Signed-off-by: Paulo Gomes <pjbgf@linux.com>

--- hack/make/deps.mk
+++ hack/make/deps.mk
@@ -3,11 +3,11 @@ GOLANGCI_VERSION = v1.60.1
 # renovate: datasource=github-release-attachments depName=k3d-io/k3d
 K3D_VERSION = v5.7.3
 
-KUBECTL_VERSION = 1.28.0
-# curk -L "https://dl.k8s.io/release/$KUBECTL_VERSION/bin/linux/arm64/kubectl.sha256"
-KUBECTL_SUM_arm64 = f5484bd9cac66b183c653abed30226b561f537d15346c605cc81d98095f1717c
-# curk -L "https://dl.k8s.io/release/$KUBECTL_VERSION/bin/linux/amd64/kubectl.sha256"
-KUBECTL_SUM_amd64 = 4717660fd1466ec72d59000bb1d9f5cdc91fac31d491043ca62b34398e0799ce
+KUBECTL_VERSION = 1.28.12
+# curl -L "https://dl.k8s.io/release/v$KUBECTL_VERSION/bin/linux/arm64/kubectl.sha256"
+KUBECTL_SUM_arm64 = f7e01dfffebb1d5811c37d558f28eefd80cbfadc0b9783b0b0ebf37c40c5c891
+# curl -L "https://dl.k8s.io/release/v$KUBECTL_VERSION/bin/linux/amd64/kubectl.sha256"
+KUBECTL_SUM_amd64 = e8aee7c9206c00062ced394418a17994b58f279a93a1be1143b08afe1758a3a2
 KUBECTL_SUM = KUBECTL_SUM_amd64
 
 HELM_VERSION = v3.15.3
