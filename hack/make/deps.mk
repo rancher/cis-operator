0e84c9a57adf68679815a77a7dfeba3f8e4240e5
vardhaman22 <vardhaman.surana@suse.com>
2025-03-03 15:15:32 +0530

    bumped kubectl and sonobuoy

--- hack/make/deps.mk
+++ hack/make/deps.mk
@@ -4,6 +4,6 @@ GOLANGCI_VERSION = v1.64.6
 K3D_VERSION = v5.8.3
 
 # TODO: Bump aligned with Rancher Manager release line
-KUBECTL_VERSION = 1.29.11
+KUBECTL_VERSION = 1.29.14
 # renovate: datasource=github-release-attachments depName=helm/helm
 HELM_VERSION = v3.17.1
