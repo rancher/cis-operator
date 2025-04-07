7a3d7300238d7c72facd8e27bf4af79fb624d8c2
vardhaman22 <vardhaman.surana@suse.com>
2025-04-07 15:12:50 +0530

    bumped kubectl and k3s version

--- hack/make/deps.mk
+++ hack/make/deps.mk
@@ -4,6 +4,6 @@ GOLANGCI_VERSION = v1.64.8
 K3D_VERSION = v5.8.3
 
 # TODO: Bump aligned with Rancher Manager release line
-KUBECTL_VERSION = 1.29.14
+KUBECTL_VERSION = 1.29.15
 # renovate: datasource=github-release-attachments depName=helm/helm
 HELM_VERSION = v3.17.2
